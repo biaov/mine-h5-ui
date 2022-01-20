3c3b01f75e329decb89389c511537d50f3233838
biaov06 <lvbiao@xcxd.com>
2022-01-20 16:03:25 +0800

    更新依赖

--- examples/components/ComboBox/interfaces.ts
+++ examples/components/ComboBox/interfaces.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 // 列表项
 export interface ListItem {
   href: string
--- examples/components/ComboBox/types.ts
+++ examples/components/ComboBox/types.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+
 import { PublicProps } from '@/types'
 import { ListItem } from './interfaces'
 
