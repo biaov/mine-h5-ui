6e9d7e7b8e75466e5aa4bc931e973dd70501e6fb
biaov <biaov@qq.com>
2025-09-10 16:28:26 +0800

    ⚙️ `useLocked` 新增业务处理方法
    Co-authored-by: biaov <biaov2017@qq.com>

--- packages/MeComposable/useLocked/index.ts
+++ packages/MeComposable/useLocked/index.ts
@@ -10,11 +10,20 @@ import type { USELocked } from './types'
     // 业务操作
     // ...
   })
+
+  const onSubmit = useLocked(async () => {
+    // 业务操作
+    // ...
+  }, async () => {
+    // 验证操作
+    // ...
+  })
  * ```
  */
-export const useLocked = (handler: USELocked.Option): USELocked.Option => {
+export const useLocked = (handler: USELocked.Option, validHandler?: () => Promise<boolean>): USELocked.Option => {
   let locked = false
   return async function (this: unknown, ...args) {
+    if (validHandler && !(await validHandler())) return
     if (locked) return
     locked = true
     try {
